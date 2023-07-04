8d59385c0cd4129b69934b1faf8dd0e6a82139f5
Najel-A <59577073+Najel-A@users.noreply.github.com>
2023-07-03 19:47:44 -0700

    Updating navbar

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../App.css';
+import BottomBar from '../BottomBar';
+
+function Home() {
+  return (
+    <>
+    <h1>HEY</h1>
+      <BottomBar />
+    </>
+  );
+}
+
+export default Home;
