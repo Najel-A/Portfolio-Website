b0c39473c5b050dc10920d4d96ebb329a566e2da
Najel-A <59577073+Najel-A@users.noreply.github.com>
2023-07-12 21:58:40 -0700

    Fix button links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+          <Link to='https://najel-a.github.io/Portfolio-Website/' className='navbar-logo' onClick={closeMobileMenu}>
             Najel
             <i class="fa-solid fa-bowl-rice fa-fade"></i>
           </Link>
@@ -37,17 +37,17 @@ function Navbar() {
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='https://najel-a.github.io/Portfolio-Website/' className='nav-links' onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='https://najel-a.github.io/Portfolio-Website/about' className='nav-links' onClick={closeMobileMenu}>
                 About
               </Link>
             </li>
             <li className='nav-item'>
-              <Link to='/work' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='https://najel-a.github.io/Portfolio-Website/work' className='nav-links' onClick={closeMobileMenu}>
                 Work
               </Link>
             </li>
