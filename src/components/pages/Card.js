7accc3d213333ce3744beaeb201f7b10c4dbd6ee
Najel Alarcon <najelalarcon@gmail.com>
2023-10-25 19:46:10 -0700

    Added some styling changes

--- src/components/pages/Card.js
+++ src/components/pages/Card.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
 const Card = ({ imageUrl, name, description, githubLink }) => {
+  const buttonStyle = {
+    background: 'green',
+    color: 'white', 
+    padding: '10px 20px',
+    borderRadius: '5px',
+    textDecoration: 'none',
+  };
+
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+
+  const buttonContainerStyle = {
+    marginTop: '20px',
+    marginBottom: '10px'
+  };
+
   return (
-    <div className="card">
+    <div className="card" style={containerStyle}>
       <img src={imageUrl} alt={name} height={250} />
       <h2>{name}</h2>
       <p>{description}</p>
-      <a href={githubLink} target="_blank" rel="noopener noreferrer">
-        GitHub
-      </a>
+      <div style={buttonContainerStyle}>
+        <a href={githubLink} target="_blank" rel="noopener noreferrer" style={buttonStyle}>
+          GitHub
+        </a>
+      </div>
     </div>
   );
 };
